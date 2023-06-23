d5e11e2f2bbc26866cdac4ac69209c04fab2625b
Grzegorz <85482129+TheBlackSurf@users.noreply.github.com>
2023-06-23 08:56:00 +0200

    update

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.aboutPage, name='about'),
+    path('contact/', views.contactPage, name='contact'),
+    path('home/', views.homePage, name='home'),
 ]
 
 
--- core/views.py
+++ core/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
+from django_htmx.http import HttpResponseLocation
 # Create your views here.
 
 def index(request):
+    if request.htmx:
+        return HttpResponseLocation(target="#website-content")
     return render(request, 'index.html')
+
+
+def aboutPage(request):
+    return render(request, 'about-page.html')
+        
+
+def contactPage(request):
+    return render(request, 'contact-page.html')
+        
+def homePage(request):
+    return render(request, 'home-page.html')
